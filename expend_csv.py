2ae5fbc0153cc5c4a65b2af41efc798bb0fea470
Yichang Liu <liuyichang041006@icloud.com>
2026-09-21 15:38:20 +1000

    Test Render capacity with 3k dataset

--- expend_csv.py
+++ expend_csv.py
@@ -2,18 +2,18 @@ import pandas as pd
 
 df = pd.read_csv("Merged_Business_Data.csv", encoding="utf-8-sig")
 
-target_n = 2000
+target_n = 3000
 
-df_2k = df.sample(
+df_nk = df.sample(
     n=target_n,
     replace=True,
     random_state=42
 ).reset_index(drop=True)
 
-df_2k.to_csv(
+df_nk.to_csv(
     "Merged_Business_Data.csv",
     index=False,
     encoding="utf-8-sig"
 )
 
-print(len(df_2k))
+print(len(df_nk))
